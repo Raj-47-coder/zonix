c4869e2abf83b999db74f74fc0cc304fe3730f10
jenishm05 <jenishmangukiya05@gmail.com>
2024-02-10 12:03:33 +0530

    Done home page banner

--- src/Inauth/Bannerslider/Bannerslider.js
+++ src/Inauth/Bannerslider/Bannerslider.js
@@ -2,10 +2,15 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import banner_img from '../../images/banner_img.png';
+import banner_img from '../../images/banner_img.jpg';
+import banner_img2 from '../../images/material-cement.png';
+import banner_img3 from '../../images/material-brick.png';
+import banner_img4 from '../../images/material-steel.png';
+import banner_img5 from '../../images/material-metal.png';
 import { Link } from "react-router-dom";
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import './Bannerslider.scss';
 
 function Bannerslider() {
@@ -23,17 +28,18 @@ function Bannerslider() {
           loop={true}
           pagination={false}
           className='banner_slider'
+          navigation={true}
         >
           <SwiperSlide>
             <Box component={'div'} className='banner_card'>
               <Box component={"div"} className='card_img'>
-                <img src={banner_img} alt={banner_img} />
+                <img src={banner_img2} alt={banner_img} />
               </Box>
               <Box component={"div"} className='card_details'>
                 {/* <Typography component={'p'}>Find great deals on ebay for Wallpaper</Typography> */}
                 <Typography component={'h3'}>Buy Online building Materials</Typography>
                 <Typography component={'p'}>There are many variations of passagesges be Lorem Lorem Ipsum available</Typography>
-                <Link component={'a'} to='/store' className='btn_banner'>Shop Now</Link>
+                <Link component={'a'} to='/store' className='btn_banner'>Book a store visit now</Link>
               </Box>
 
             </Box>
@@ -42,13 +48,13 @@ function Bannerslider() {
           <SwiperSlide>
             <Box component={'div'} className='banner_card'>
               <Box component={"div"} className='card_img'>
-                <img src={banner_img} alt={banner_img} />
+                <img src={banner_img3} alt={banner_img} />
               </Box>
               <Box component={"div"} className='card_details'>
-                <Typography component={'p'}>Find great deals on ebay for Wallpaper</Typography>
-                <Typography component={'h3'}>sponge tools</Typography>
+                {/* <Typography component={'p'}>Find great deals on ebay for Wallpaper</Typography> */}
+                <Typography component={'h3'}>Buy Manny type of Bricks</Typography>
                 <Typography component={'p'}>There are many variations of passagesges be Lorem Lorem Ipsum available</Typography>
-                <Link component={'a'} to='/store' className='btn_banner'>Shop Now</Link>
+                <Link component={'a'} to='/store' className='btn_banner'>Book a store visit now</Link>
               </Box>
 
             </Box>
@@ -57,13 +63,13 @@ function Bannerslider() {
           <SwiperSlide>
             <Box component={'div'} className='banner_card'>
               <Box component={"div"} className='card_img'>
-                <img src={banner_img} alt={banner_img} />
+                <img src={banner_img4} alt={banner_img} />
               </Box>
               <Box component={"div"} className='card_details'>
-                <Typography component={'p'}>Find great deals on ebay for Wallpaper</Typography>
-                <Typography component={'h3'}>sponge tools</Typography>
-                <Typography component={'p'}>There are many variations of passagesges be Lorem Lorem Ipsum available</Typography>
-                <Link component={'a'} to='/store' className='btn_banner'>Shop Now</Link>
+                {/* <Typography component={'p'}>Find great deals on ebay for Wallpaper</Typography> */}
+                <Typography component={'h3'}>Steels</Typography>
+                <Typography component={'p'}>Explore  the range of choices @zonixz</Typography>
+                <Link component={'a'} to='/store' className='btn_banner'>Book a store visit now</Link>
               </Box>
 
             </Box>
@@ -72,43 +78,13 @@ function Bannerslider() {
           <SwiperSlide>
             <Box component={'div'} className='banner_card'>
               <Box component={"div"} className='card_img'>
-                <img src={banner_img} alt={banner_img} />
+                <img src={banner_img5} alt={banner_img} />
               </Box>
               <Box component={"div"} className='card_details'>
-                <Typography component={'p'}>Find great deals on ebay for Wallpaper</Typography>
-                <Typography component={'h3'}>sponge tools</Typography>
-                <Typography component={'p'}>There are many variations of passagesges be Lorem Lorem Ipsum available</Typography>
-                <Link component={'a'} to='/store' className='btn_banner'>Shop Now</Link>
-              </Box>
-
-            </Box>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Box component={'div'} className='banner_card'>
-              <Box component={"div"} className='card_img'>
-                <img src={banner_img} alt={banner_img} />
-              </Box>
-              <Box component={"div"} className='card_details'>
-                <Typography component={'p'}>Find great deals on ebay for Wallpaper</Typography>
-                <Typography component={'h3'}>sponge tools</Typography>
-                <Typography component={'p'}>There are many variations of passagesges be Lorem Lorem Ipsum available</Typography>
-                <Link component={'a'} to='/store' className='btn_banner'>Shop Now</Link>
-              </Box>
-
-            </Box>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Box component={'div'} className='banner_card'>
-              <Box component={"div"} className='card_img'>
-                <img src={banner_img} alt={banner_img} />
-              </Box>
-              <Box component={"div"} className='card_details'>
-                <Typography component={'p'}>Find great deals on ebay for Wallpaper</Typography>
-                <Typography component={'h3'}>sponge tools</Typography>
-                <Typography component={'p'}>There are many variations of passagesges be Lorem Lorem Ipsum available</Typography>
-                <Link component={'a'} to='/store' className='btn_banner'>Shop Now</Link>
+                {/* <Typography component={'p'}>Find great deals on ebay for Wallpaper</Typography> */}
+                <Typography component={'h3'}>Metals</Typography>
+                <Typography component={'p'}>Explore  the range of choices @zonixz</Typography>
+                <Link component={'a'} to='/store' className='btn_banner'>Book a store visit now</Link>
               </Box>
 
             </Box>
--- src/Inauth/Topcategory/Topcategory.js
+++ src/Inauth/Topcategory/Topcategory.js
@@ -17,98 +17,96 @@ function Topcategory() {
   return (
     <>
       <Box component={'div'} className="topCategory_box">
-        <Box component={'div'} className='container'>
-          <Box component={'div'} className="tocategory_row">
-            <Box component={'div'} className="tocategory_cols">
-              <Typography component={'h6'}>Make your bright home | up to 60% off</Typography>
+        <Box component={'div'} className="tocategory_row">
+          <Box component={'div'} className="tocategory_cols">
+            <Typography component={'h6'}>Make your bright home | up to 60% off</Typography>
 
-              <Box component={'div'} className="inner_cardRow">
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={concert} alt={'concert'} />
-                  <Typography component={'p'}>Brick</Typography>
-                </NavLink>
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={concert} alt={'concert'} />
-                  <Typography component={'p'}>Brick</Typography>
-                </NavLink>
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={concert} alt={'concert'} />
-                  <Typography component={'p'}>Brick</Typography>
-                </NavLink>
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={concert} alt={'concert'} />
-                  <Typography component={'p'}>Brick</Typography>
-                </NavLink>
-              </Box>
+            <Box component={'div'} className="inner_cardRow">
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={brick} alt={'concert'} />
+                <Typography component={'p'}>Brick</Typography>
+              </NavLink>
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={brick} alt={'concert'} />
+                <Typography component={'p'}>Brick</Typography>
+              </NavLink>
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={brick} alt={'concert'} />
+                <Typography component={'p'}>Brick</Typography>
+              </NavLink>
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={brick} alt={'concert'} />
+                <Typography component={'p'}>Brick</Typography>
+              </NavLink>
             </Box>
+          </Box>
 
-            <Box component={'div'} className="tocategory_cols">
-              <Typography component={'h6'}>Make your bright home</Typography>
+          <Box component={'div'} className="tocategory_cols">
+            <Typography component={'h6'}>Make your bright home</Typography>
 
-              <Box component={'div'} className="inner_cardRow">
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={cement} alt={'concert'} />
-                  <Typography component={'p'}>Cement</Typography>
-                </NavLink>
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={cement} alt={'concert'} />
-                  <Typography component={'p'}>Cement</Typography>
-                </NavLink>
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={cement} alt={'concert'} />
-                  <Typography component={'p'}>Cement</Typography>
-                </NavLink>
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={cement} alt={'concert'} />
-                  <Typography component={'p'}>Cement</Typography>
-                </NavLink>
-              </Box>
+            <Box component={'div'} className="inner_cardRow">
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={cement} alt={'concert'} />
+                <Typography component={'p'}>Cement</Typography>
+              </NavLink>
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={cement} alt={'concert'} />
+                <Typography component={'p'}>Cement</Typography>
+              </NavLink>
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={cement} alt={'concert'} />
+                <Typography component={'p'}>Cement</Typography>
+              </NavLink>
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={cement} alt={'concert'} />
+                <Typography component={'p'}>Cement</Typography>
+              </NavLink>
             </Box>
+          </Box>
 
-            <Box component={'div'} className="tocategory_cols">
-              <Typography component={'h6'}>Make your bright home</Typography>
+          <Box component={'div'} className="tocategory_cols">
+            <Typography component={'h6'}>Make your bright home</Typography>
 
-              <Box component={'div'} className="inner_cardRow">
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={wood} alt={'concert'} />
-                  <Typography component={'p'}>Wood</Typography>
-                </NavLink>
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={wood} alt={'concert'} />
-                  <Typography component={'p'}>Wood</Typography>
-                </NavLink>
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={wood} alt={'concert'} />
-                  <Typography component={'p'}>Wood</Typography>
-                </NavLink>
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={wood} alt={'concert'} />
-                  <Typography component={'p'}>Wood</Typography>
-                </NavLink>
-              </Box>
+            <Box component={'div'} className="inner_cardRow">
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={wood} alt={'concert'} />
+                <Typography component={'p'}>Wood</Typography>
+              </NavLink>
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={wood} alt={'concert'} />
+                <Typography component={'p'}>Wood</Typography>
+              </NavLink>
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={wood} alt={'concert'} />
+                <Typography component={'p'}>Wood</Typography>
+              </NavLink>
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={wood} alt={'concert'} />
+                <Typography component={'p'}>Wood</Typography>
+              </NavLink>
             </Box>
+          </Box>
 
-            <Box component={'div'} className="tocategory_cols">
-              <Typography component={'h6'}>Make your bright home</Typography>
+          <Box component={'div'} className="tocategory_cols">
+            <Typography component={'h6'}>Make your bright home</Typography>
 
-              <Box component={'div'} className="inner_cardRow">
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={metal} alt={'concert'} />
-                  <Typography component={'p'}>metal</Typography>
-                </NavLink>
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={metal} alt={'concert'} />
-                  <Typography component={'p'}>metal</Typography>
-                </NavLink>
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={metal} alt={'concert'} />
-                  <Typography component={'p'}>metal</Typography>
-                </NavLink>
-                <NavLink to={'/'} component={'a'} className="inner_card">
-                  <img src={metal} alt={'concert'} />
-                  <Typography component={'p'}>metal</Typography>
-                </NavLink>
-              </Box>
+            <Box component={'div'} className="inner_cardRow">
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={metal} alt={'concert'} />
+                <Typography component={'p'}>metal</Typography>
+              </NavLink>
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={metal} alt={'concert'} />
+                <Typography component={'p'}>metal</Typography>
+              </NavLink>
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={metal} alt={'concert'} />
+                <Typography component={'p'}>metal</Typography>
+              </NavLink>
+              <NavLink to={'/'} component={'a'} className="inner_card">
+                <img src={metal} alt={'concert'} />
+                <Typography component={'p'}>metal</Typography>
+              </NavLink>
             </Box>
           </Box>
         </Box>
