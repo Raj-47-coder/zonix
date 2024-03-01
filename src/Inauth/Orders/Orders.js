50266180d0cbbd7c3dab0181bacbba9d4f8fb220
jenishm05 <jenishmangukiya05@gmail.com>
2024-03-01 14:03:22 +0530

    Update Desing add tracking section

--- src/Inauth/Orders/Orders.js
+++ src/Inauth/Orders/Orders.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Typography, Checkbox, FormControlLabel, Button } from "@mui/material";
+import { Box, Typography, Checkbox, FormControlLabel, Button, Stepper, Step, StepLabel } from "@mui/material";
 import OrderImg from '../../images/brick.png';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
@@ -12,16 +12,19 @@ const orderList = [
     orderPrice: '1099',
     orderStatus: 'success',
   },
-  {
-    orderimg: OrderImg,
-    orderName: 'Indian standard Quality Chips Building Material, Grade Standard:',
-    orderPrice: '599',
-    orderStatus: 'danger',
-  },
+  // {
+  //   orderimg: OrderImg,
+  //   orderName: 'Indian standard Quality Chips Building Material, Grade Standard:',
+  //   orderPrice: '599',
+  //   orderStatus: 'danger',
+  // },
 ]
+
+const steps = ['Order Comfirmed', 'Shipped', 'Out of Delivery', 'Step 4'];
 
 export default function Orders() {
   const [profile, setProfile] = React.useState('');
+  const [activeStep, setActiveStep] = React.useState(0);
 
   const handleprofile = () => {
     if (profile === '') {
@@ -30,6 +33,14 @@ export default function Orders() {
       setProfile('')
     }
   }
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
   return (
     <>
       <Box component={'div'} className='container'>
@@ -92,25 +103,45 @@ export default function Orders() {
             <Button className='profile_leftBtn' onClick={handleprofile}>{profile === '' ? <KeyboardArrowRightIcon /> : <KeyboardArrowLeftIcon />}</Button>
 
             {orderList.map((items, i) => (
-              <Box component={'div'} className='order_card'>
-                <Box component={'div'} className='orders_item'>
-                  <img src={items.orderimg} alt='Brick' />
-                  <Typography component={'p'}>{items.orderName}</Typography>
-                </Box>
+              <>
+                <Box component={'div'} className='order_card'>
+                  <Box component={'div'} className='orders_item'>
+                    <img src={items.orderimg} alt='Brick' />
+                    <Typography component={'p'}>{items.orderName}</Typography>
+                  </Box>
 
-                <Box component={'div'} className='orders_price'>
-                  <Typography component={'p'}>₹{items.orderPrice}</Typography>
-                </Box>
+                  <Box component={'div'} className='orders_price'>
+                    <Typography component={'p'}>₹{items.orderPrice}</Typography>
+                  </Box>
 
-                <Box component={'div'} className='orders_status'>
-                  <Typography component={'h6'} className={`status ${items.orderStatus}`}>Delivered on Dec 14, 2023</Typography>
-                  {items.orderStatus === 'success' ? <Typography component={'p'}>Your item has been delivered</Typography> : ''}
+                  <Box component={'div'} className='orders_status'>
+                    <Typography component={'h6'} className={`status ${items.orderStatus}`}>Delivered on Dec 14, 2023</Typography>
+                    {items.orderStatus === 'success' ? <Typography component={'p'}>Your item has been delivered</Typography> : ''}
+                  </Box>
                 </Box>
-              </Box>
+                <Box component={'div'} className='order_tracking'>
+                  <Stepper activeStep={activeStep} alternativeLabel>
+                    {steps.map((label, index) => (
+                      <Step key={index}>
+                        <StepLabel>{label}</StepLabel>
+                      </Step>
+                    ))}
+                  </Stepper>
+
+                  {/* <div style={{ marginTop: 20 }}>
+                    <Button disabled={activeStep === 0} onClick={handleBack}>
+                      Back
+                    </Button>
+                    <Button variant="contained" color="primary" onClick={handleNext}>
+                      {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                    </Button>
+                  </div> */}
+                </Box>
+              </>
             ))}
           </Box>
-        </Box>
-      </Box>
+        </Box >
+      </Box >
     </>
   );
 }
